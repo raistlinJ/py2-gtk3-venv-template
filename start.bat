504ad2cb7faf42e37e3448340a8a3ebd1b3e425b
raistlinJ <raistlinJ@users.noreply.github.com>
2018-06-27 14:39:59 -0600

    Removed uneeded file

--- start.bat
+++
@@ -1,5 +0,0 @@
-REM the name of the container used during installation 
-set VENV_NAME=app-container 
- 
-REM activate the container and invoke the gui 
-app-container\Scripts\activate & cd src & python main.py & deactivate & cd .. 
